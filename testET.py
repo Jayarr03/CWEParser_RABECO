4bab205233af7a6887faf81b25546cb46f505705
dstog97 <franzmueller800492@gmail.com>
2023-09-24 19:02:11 +0200

    Added element tree parsing

--- testET.py
+++ testET.py
@@ -3,16 +3,21 @@ import xml.etree.ElementTree as ET
 
 def main():
     tree = ET.parse("testing\\cwec_v4.12.xml")
+    cwe = open("cwe.json", "w")
+    cwe.write("{\"CWE\":[")
     root = tree.getroot()
     weaknesses = root.find("{http://cwe.mitre.org/cwe-7}Weaknesses")
     for weakness in weaknesses:
-        print(weakness.attrib)
+        id_name = "{{\"id\":\"{}\", \"name\": \"{}\", ".format(weakness.attrib["ID"], weakness.attrib["Name"].replace("\\", "\\\\"))
         for des in weakness:
             if "{http://cwe.mitre.org/cwe-7}Description" == des.tag:
-                print(des.text)
-
-
-
+                tmp_des = des.text
+                tmp_des = tmp_des.replace("\\", "\\\\").replace("\"", "\\\"")
+                id_name_des = id_name + "\"description\":\"{}\"}},".format(tmp_des).\
+                    replace("\n", " ").\
+                    replace(u"\u0009", " ") + "\n"
+                cwe.write(id_name_des)
+    cwe.write("{}]}")
 
 
 if __name__ == "__main__":
