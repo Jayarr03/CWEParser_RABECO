5fd6befd96bb8a729b020cbdbc82eaf036ed6bf4
dstog97 <franzmueller800492@gmail.com>
2023-09-24 19:29:11 +0200

    Cleanup

--- etparser.py
+++ etparser.py
@@ -6,22 +6,6 @@ import parserUtils
 def main():
     parser = parserUtils.ElementTreeParser()
     parser.parseCWE()
-    # tree = ET.parse("testing\\cwec_v4.12.xml")
-    # cwe = open("cwe.json", "w")
-    # cwe.write("{\"CWE\":[")
-    # root = tree.getroot()
-    # weaknesses = root.find("{http://cwe.mitre.org/cwe-7}Weaknesses")
-    # for weakness in weaknesses:
-    #     id_name = "{{\"id\":\"{}\", \"name\": \"{}\", ".format(weakness.attrib["ID"], weakness.attrib["Name"].replace("\\", "\\\\"))
-    #     for des in weakness:
-    #         if "{http://cwe.mitre.org/cwe-7}Description" == des.tag:
-    #             tmp_des = des.text
-    #             tmp_des = tmp_des.replace("\\", "\\\\").replace("\"", "\\\"")
-    #             id_name_des = id_name + "\"description\":\"{}\"}},".format(tmp_des).\
-    #                 replace("\n", " ").\
-    #                 replace(u"\u0009", " ") + "\n"
-    #             cwe.write(id_name_des)
-    # cwe.write("{}]}")
 
 
 if __name__ == "__main__":
